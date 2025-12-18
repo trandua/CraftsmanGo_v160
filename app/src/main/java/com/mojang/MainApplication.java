0c8d7a1bbd01d5f7604f08b5b30ead3f47b155a8
Trần Đua <trandua@gmail.com>
2025-12-18 14:31:06 +0700

    1
    1

--- app/src/main/java/com/mojang/MainApplication.java
+++ app/src/main/java/com/mojang/MainApplication.java
@@ -4,9 +4,9 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 
-//import com.boldwin.sdk.api.BoldwinSDK;
-//import com.boldwin.sdk.api.listeners.BoldwinSdkInitializationListener;
-//import com.boldwin.sdk.api.utils.BoldwinInitializationStatus;
+import com.boldwin.sdk.api.BoldwinSDK;
+import com.boldwin.sdk.api.listeners.BoldwinSdkInitializationListener;
+import com.boldwin.sdk.api.utils.BoldwinInitializationStatus;
 
 public class MainApplication extends Application {
     private String PUBLISHER_ID ="";
@@ -16,15 +16,15 @@ public class MainApplication extends Application {
         //TODO Init Solar Engine
         SolarEngine.initEngine(getApplicationContext());
 
-//        BoldwinSDK.initialize(PUBLISHER_ID, getApplicationContext(), new BoldwinSdkInitializationListener() {
-//            @Override
-//            public void onInitializationComplete(@NonNull BoldwinInitializationStatus status) {
-//                if(status == BoldwinInitializationStatus.SUCCEEDED){
-//                    System.out.println("SDK initialized successfully!");
-//                }else{
-//                    System.out.println("SDK initialization error: $status\n${status.description}");
-//                }
-//            }
-//        });
+        BoldwinSDK.initialize(PUBLISHER_ID, getApplicationContext(), new BoldwinSdkInitializationListener() {
+            @Override
+            public void onInitializationComplete(@NonNull BoldwinInitializationStatus status) {
+                if(status == BoldwinInitializationStatus.SUCCEEDED){
+                    System.out.println("SDK initialized successfully!");
+                }else{
+                    System.out.println("SDK initialization error: $status\n${status.description}");
+                }
+            }
+        });
     }
 }
